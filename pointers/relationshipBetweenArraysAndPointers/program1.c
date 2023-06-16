003554cd20b9278ab6f198be151ca9b2fd0f2020
Adityakaldhone <adityakaldhone1357@gmail.com>
2023-06-16 09:03:32 +0530

    push

---
+++ pointers/relationshipBetweenArraysAndPointers/program1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main(){
+
+	int size;
+	printf("Enter array size\n");
+	scanf("%d",&size);
+	int arr[size];
+	printf("Enter array elements\n");
+	for(int i=0;i<size;i++){
+	
+		scanf("%d",&arr[i]);
+
+	}
+	for(int i=0;i<size;i++){
+	
+		printf("%d\n",arr[i]);
+
+	}
+	for(int i=0;i<size;i++){
+	
+		printf("%d\n",*(arr+i));
+
+	}
+
+}
